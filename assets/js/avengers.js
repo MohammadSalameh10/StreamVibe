cbb1d16e45dec8d25082a588ec662faa2ac9eaae
MohammadSalameh10 <hamodesalameh747@gmail.com>
2024-11-18 23:37:37 +0200

    responsive movies information section

--- assets/js/avengers.js
+++ assets/js/avengers.js
@@ -1,13 +1,51 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
+
     loop: true,
   
    slidesPerView: 7,
-    // Navigation arrows
+    
+   breakpoints: {
+    0: {
+        slidesPerView: 2.9,
+      },
+
+      380: {
+        slidesPerView: 3,
+      },
+      390: {
+        slidesPerView: 3.5,
+      },
+      420: {
+        slidesPerView: 4,
+      },
+      460: {
+        slidesPerView: 4.5,
+      },
+      500: {
+        slidesPerView: 5,
+      },
+      576: {
+        slidesPerView: 2.7,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 5,
+      },
+      1200: {
+        slidesPerView: 6,
+      },
+      1400: {
+        slidesPerView: 7,
+      },
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
   autoplay:true,
+
   });
